afb45d48c92bab0e795a1adbbff33852dd65c88d
tavaresflavia <altavares.flavia@gmail.com>
2023-08-11 11:02:53 -0400

    navigate

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,19 +1,21 @@
 import { useState } from "react";
 import "./Form.scss";
-import { Link } from "react-router-dom";
+import { Link, useNavigate} from "react-router-dom";
 import axios from "axios";
 import submitIcon from '../../assests/icons/submit.png'
 
 const Form = () => {
-  const todayDate = new Date();
+
+  const navigate = useNavigate();
+  
 
   const [title, setTitle] = useState("");
-  const [dueDate, setDueDate] = useState(todayDate);
+  const [dueDate, setDueDate] = useState("");
   const [priority,setPriority] = useState("2")
   const [isFun, setIsFun] = useState(false);
   const priorityArr = ["Low","Medium","High"]
 
-
+ 
 
   const handleChangeTitle = (e) => {
     setTitle(e.target.value);
@@ -31,6 +33,8 @@ const Form = () => {
   const handleChangePriority =(e)=> {
     setPriority(e.target.value);
   }
+  
+  
 
   const handleSubmission = () => {
     axios.post("http://localhost:5050" + "/item", {
@@ -39,7 +43,10 @@ const Form = () => {
     dueDate:dueDate,
     isFun: isFun
     }
+
     )
+    .then((res) => {
+    navigate('/');})
   }
 
 
@@ -56,7 +63,7 @@ const Form = () => {
         {isFun ? "🙂" : "😐"}
       </p></div>
       <div className="form__row">
-      <input className="form__due-date" type="date" value={dueDate} 
+      <input   className="form__due-date" type="date" value={dueDate} 
       onChange = {handleChangeDueDate}/>
       
       <input className= "form__range"  value = {priority}  min = "1" max ="3" type= "range" 
