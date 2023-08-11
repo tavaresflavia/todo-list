9bc21ecd5df25d3e66f8ad916efc5542e3c48f94
Mikael <s.h.koo1213@gmail.com>
2023-08-11 14:11:42 -0400

    form design change

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,21 +1,21 @@
 import { useState } from "react";
 import "./Form.scss";
-import { Link, useNavigate} from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import axios from "axios";
 import submitIcon from '../../assests/icons/submit.png'
 
 const Form = () => {
 
   const navigate = useNavigate();
-  
+
 
   const [title, setTitle] = useState("");
   const [dueDate, setDueDate] = useState("");
-  const [priority,setPriority] = useState("2")
+  const [priority, setPriority] = useState("2")
   const [isFun, setIsFun] = useState(false);
-  const priorityArr = ["Low","Medium","High"]
+  const priorityArr = ["Low", "Medium", "High"]
 
- 
+
 
   const handleChangeTitle = (e) => {
     setTitle(e.target.value);
@@ -30,51 +30,67 @@ const Form = () => {
     setIsFun(newValue);
   };
 
-  const handleChangePriority =(e)=> {
+  const handleChangePriority = (e) => {
     setPriority(e.target.value);
   }
-  
-  
+
+
 
   const handleSubmission = () => {
     axios.post("http://localhost:5050" + "/item", {
-    title: title,
-    priority: priority,
-    dueDate:dueDate,
-    isFun: isFun
+      title: title,
+      priority: priorityArr[priority - 1],
+      dueDate: dueDate,
+      isFun: isFun
     }
 
     )
-    .then((res) => {
-    navigate('/');})
+      .then((res) => {
+        navigate('/');
+      })
   }
 
 
   return (
     <div className="form">
-      <div className="form__row"><input
-        className="form__title"
-        value={title}
-        type="text"
-        onChange={handleChangeTitle}
-      />
-      <p className="form__smile" onClick={handleChangeFun}>
-        {isFun ? "🙂" : "😐"}
-      </p></div>
       <div className="form__row">
-      <input   className="form__due-date" type="date" value={dueDate} 
-      onChange = {handleChangeDueDate}/>
-      
-      <input className= "form__range"  value = {priority}  min = "1" max ="3" type= "range" 
-      onChange= {handleChangePriority}/>
-      <span className = {"form__priority form__priority--" + (priorityArr[priority-1]) }>{priorityArr[priority-1]} </span>
+        <div className="form__title-box">
+          <label htmlFor="title" class="form__label">
+            Task :
+          </label>
+          <input
+            name="title"
+            className="form__title"
+            value={title}
+            type="text"
+            onChange={handleChangeTitle}
+          />
+        </div>
+        <p className="form__smile" onClick={handleChangeFun}>
+          {isFun ? "🙂" : "😐"}
+        </p></div>
+      <div className="form__row2">
+        <div className="form__date-box">
+          <label htmlFor="date" className="">Date :</label>
+          <input name="date" className="form__due-date" type="date" value={dueDate}
+            onChange={handleChangeDueDate} />
+        </div>
+        <div className="form__priority-div">
+          <label htmlFor="priority" className="form__priority-label">Priority :</label>
+          <div class="form__priority-select">
+            <input name="priority" className="form__range" value={priority} min="1" max="3" type="range"
+              onChange={handleChangePriority} />
+            <span className={"form__priority form__priority--" + (priorityArr[priority - 1])}>{priorityArr[priority - 1]} </span>
+          </div>
+        </div>
       </div>
 
-      <div className = "form__btns">
-      <Link className="form__cancel" to = '/'> Cancel </Link>
+      <div className="form__btns">
+        <Link className="form__cancel" to='/'> Cancel </Link>
 
-      <img src={submitIcon}className="form__submit" onClick= 
-      {handleSubmission} ></img></div>
+        <img src={submitIcon} className="form__submit" onClick=
+          {handleSubmission} ></img>
+      </div>
     </div>
   );
 };
--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -11,8 +11,6 @@ const Item = ({title,dueDate,priority,id, isFun }) => {
 const [hidden,setHidden] = useState(false)
 
     const handleCheck = () => {
-        console.log(`${SERVER_URL}/item/${id}`);
-
         axios.delete(`${SERVER_URL}/item/${id}`)
         .then(() => setHidden(true)
         )
