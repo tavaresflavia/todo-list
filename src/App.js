a6011c3693776bae8d41ba9c0487a1887ba7a857
Mikael <s.h.koo1213@gmail.com>
2023-08-11 10:35:35 -0400

    add feature/header

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import "./App.css";
+import "./App.scss";
 import Header from "./components/Header/Header";
 import HomePage from "./pages/HomePage/HomePage";
 import FormPage from "./pages/FormPage/FormPage";
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,52 @@
 import './Header.scss'
+import {Link} from 'react-router-dom';
+import logo from "../../assets/images/logo.jpg"
+import plusIcon from "../../assets/icons/plus1.png"
 
 const Header = () => {
     return (
-        <div>
-            
-        </div>
+        <header className="app-header">
+            <img 
+                src={logo} 
+                alt="logo for Taskfusion" 
+                className="main-logo"
+            />
+
+            <nav className='nav'>
+                <ul className='nav__list'>
+                    <li className='nav__list-item'>
+                        <Link
+                            to="/prioritylist"
+                            className='nav__link'
+                        >
+                        Daily
+                        </Link>
+                    </li>
+
+                    <li className='nav__list-item'>
+                        <Link
+                            to="/prioritylist"
+                            className='nav__link'
+                        >
+                        Priority
+                        </Link>
+                    </li>
+
+                    <li className='nav__list-item'>
+                        <Link
+                            to="/form"
+                            className='nav__link'>
+                            <img 
+                                src={plusIcon} 
+                                alt="plus icon" 
+                                className="plus-icon"
+                            />
+                        </Link>
+                    </li>
+                </ul>
+
+            </nav>
+        </header>
     );
 };
 
